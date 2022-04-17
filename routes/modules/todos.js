8b1958f6d1472b90631b499dbd6144e0d7bd4bea
tkooooo123 <tkooooo123@gmail.com>
2022-04-17 23:48:10 +0800

    add edit feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,17 +4,21 @@ const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
 
+router.get('/new', (req, res) => {
+    return res.render('new')
+})
+
 router.get('/:id', (req, res) => {
     const UserId = req.user.id
     const id = req.params.id
-    return Todo.findOne({ id, UserId })
+    return Todo.findOne({
+    where: { id, UserId }
+  })
         .then(todo => res.render('detail', { todo: todo.toJSON() }))
         .catch(error => console.log(error))
 })
 
-router.get('/new', (req, res) => {
-    return res.render('new')
-})
+
 
 router.post('/', (req, res) => {
     const UserId = req.user.id
@@ -25,4 +29,28 @@ router.post('/', (req, res) => {
         .catch(error => console.log(error))
 })
 
+router.get('/:id/edit', (req, res) => {
+    const UserId = req.user.id
+    const id = req.params.id
+  
+    return Todo.findOne({ where: { id, UserId } })
+      .then(todo => res.render('edit', { todo: todo.get() }))
+      .catch(error => console.log(error))
+  })
+  
+  router.put('/:id', (req, res) => {
+    const UserId = req.user.id
+    const id = req.params.id
+    const { name, isDone } = req.body
+    console.log(req.body)
+  
+    return Todo.findOne({ where: { id, UserId } })
+      .then(todo => {
+        todo.name = name
+        todo.isDone = isDone === 'on'
+        return todo.save()
+      })
+      .then(() => res.redirect(`/todos/${id}`))
+      .catch(error => console.log(error))
+  })
 module.exports = router
