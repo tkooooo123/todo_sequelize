53f0a682f87f08f993ce14b016ce393c609a091a
tkooooo123 <tkooooo123@gmail.com>
2022-04-17 23:13:51 +0800

    add message partials

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
 const usePassport = require('./config/passport')
 const routes = require('./routes')
+const flash = require('connect-flash')
 
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -23,14 +21,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash()) 
 app.use((req, res, next) => {
     // 你可以在這裡 console.log(req.user) 等資訊來觀察
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')  
+  res.locals.warning_msg = req.flash('warning_msg')
     next()
   })
-  
+
 app.use(routes)
 
 
